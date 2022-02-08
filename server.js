2ebc6f148d426a986da9525337f494b9868b3989
rinokapi <rynnoapi.02@gmail.com>
2022-02-08 16:42:47 +0800

    create another pages

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log("hello-world")
+// require the dependecies
+const bodyParser = require("body-parser");
+const express = require("express");
+
+// define express()
+const app = express();
+
+// static path
+app.use("/public", express.static(__dirname + "/public"));
+
+// log the request
+app.use((req, res, next) => {
+	const log = req.method + " " + req.originalUrl + " on " + Date().slice(0, 24);
+	next(console.log(`${log}\n`));
+});
+
+// routes
+app.get("/", (req, res) => {
+	// res.send("welcome to express ☕☕☕");
+	res.sendFile(__dirname + "/src/views/index.html");
+});
+
+// error handler
+app.use((req, res, next) => {
+	res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
+
+// server up
+const port = process.env.port || 3000;
+app.listen(port, () =>
+	console.log(`\nserver is running on port ${port}...\n🔥🔥🔥\n`)
+);
